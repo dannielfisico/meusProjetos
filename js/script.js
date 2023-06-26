bd14f49f138d08a27ffa23de921906a2da51a15a
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-25 22:01:26 -0300

    projeto concluido

--- js/script.js
+++ js/script.js
@@ -44,7 +44,10 @@ function fazerJogada(celula){
     //Salvar jogada no array jogadasFeitas
     jogadasFeitas[celula.target.id] = simbolos[jogadorAtual]
     console.log(jogadasFeitas)
+    eEmpate()
+    eCampeao()
     trocarJogador()
+    
     //Trocar informação do jogador atual, alternando entre um e outro
     document.querySelector(".info h1:nth-child(1)").innerHTML = `Jogador atual: ${simbolos[jogadorAtual]}`
     //Trocar informação Quantidade de jogadas realizadas
@@ -57,3 +60,45 @@ function trocarJogador(){
     qtdJogadas++
 }
 
+//Função que verifica se o jogador foi campeao
+
+function eCampeao(){
+    const posicoesCampeans = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    for(let i = 0; i < posicoesCampeans.length; i++){
+       let sequencia = posicoesCampeans[i]
+
+       let pos0 = sequencia[0]
+       let pos1 = sequencia[1]
+       let pos2 = sequencia[2]
+
+       if(jogadasFeitas[pos0]== jogadasFeitas[pos1] && jogadasFeitas[pos0] == jogadasFeitas[pos2] && jogadasFeitas[pos0] != ""){
+            document.querySelector('.tela-resultado').style.display = "flex"
+            document.querySelector('.tela-resultado h1').innerHTML = `${simbolos[jogadorAtual]} Venceu!`
+            return true
+       }
+    }
+
+
+    return false
+}
+
+//Função para verificar se é empate
+
+function eEmpate(){
+    if(!eCampeao() && jogadasFeitas.indexOf("") == -1){
+        document.querySelector('.tela-resultado').style.display = "flex"
+        document.querySelector('.tela-resultado h1').innerHTML = `Empate`
+        return true
+    }
+    return false
+}
