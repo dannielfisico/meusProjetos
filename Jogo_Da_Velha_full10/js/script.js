590189ed47696f0cad1a5d122db4b6c5d789678d
dannielfisico <treinamentosinterativa@gmail.com>
2023-07-09 20:55:17 -0300

    iniciando variáveis

---
+++ Jogo_Da_Velha_full10/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const jogadas = ["", "", "","", "", "","", "", ""]
+    const simbols = ['🎃', "🐄"]
+    let player = 0
+
+    
+
+})
+
+
+
+
