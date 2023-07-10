a99df4a833e195c8a7debe1401b96462f0acbe0b
dannielfisico <treinamentosinterativa@gmail.com>
2023-07-10 09:23:33 -0300

    iniciando variáveis

--- Jogo_Da_Velha_full10/js/script.js
+++ Jogo_Da_Velha_full10/js/script.js
@@ -3,7 +3,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const simbols = ['🎃', "🐄"]
     let player = 0
 
-    
+     //Selecionar todas as celulas do board
+    const celulas = document.querySelectorAll('.celula')
+
+    //Função fazerJogada
+    const fazerJogada = (evento) => {
+        evento.target.innerHTML = simbols[player]
+        
+        // jogadas[celula.target.id] = simbols[player]
+        // salvarJogada
+        console.log(evento.target.id)
+    }
+
+    //Função salvarJogada
+    const salvarJogada = (evento) => {
+      jogadas[evento.target.id] = simbols[player]
+      console.log(jogadas)
+    }
+
+   
+
+    celulas.forEach(celula => {
+        celula.addEventListener('click', fazerJogada, salvarJogada, {once: true})
+    })
+
+     
+     //Adicionar evendo de clique nas celulas
+  
+
 
 })
 
